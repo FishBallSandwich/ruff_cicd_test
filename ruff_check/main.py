8dc3e649b1e322d2b8473bf44d1812d53259210d
FishBallSandwich <120811696+FishBallSandwich@users.noreply.github.com>
2024-09-01 16:45:17 +0200

    init commit

---
+++ ruff_check/main.py
@@ -0,0 +1,12 @@
+import random
+
+
+
+def rng():
+    rand_int = random.randint(1, 10)
+    print(rand_int)
+
+
+def rand_choice():
+    mylist = ["Ajay", "Davide", "Keanu", "Jojo"]
+    print(random.choice(mylist))
---
+++ ruff_skip/test.py
@@ -0,0 +1,25 @@
+import random
+import json
+
+
+
+
+def main():
+    print('ruff dont lint this please')
+
+
+def horrible_styling():
+
+
+
+
+
+
+    print('horrible')
+    print('styling')
+
+
+def syntax_error():
+
+    print(i_am_an_error)
+
